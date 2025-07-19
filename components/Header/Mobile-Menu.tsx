a7bcfaad7214018350ed9f9e31bca68bff8db6bf
joysuperman <joymojumder529@gmail.com>
2025-07-20 03:36:17 +0600

    error Fix

--- components/Header/Mobile-Menu.tsx
+++ components/Header/Mobile-Menu.tsx
@@ -1,33 +1,33 @@
-"use client";
-import React from 'react'
-import { useState } from 'react';
-import Link from 'next/link';
+    "use client";
+    import React from 'react'
+    import { useState } from 'react';
+    import Link from 'next/link';
 
-export default function MobileMenu({ items: [] }) {
-    const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div className='md:hidden'>
-        <button className="btn btn-square btn-ghost" onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? (
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current" onClick={() => setIsOpen(false)}>
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-                </svg>
-            ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+    export default function MobileMenu({ items = [] }) {
+        const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className='md:hidden'>
+            <button className="btn btn-square btn-ghost" onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current" onClick={() => setIsOpen(false)}>
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+                )}
+            </button>
+
+            {isOpen && (
+                <ul className="absolute z-10 left-0 right-0 w-full top-16 bg-base-100 p-2">
+                    {items.map((item) => (
+                        <li key={item.title}>
+                            <Link href={item.slag} onClick={() => setIsOpen(false)}>
+                                {item.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             )}
-        </button>
-
-        {isOpen && (
-            <ul className="absolute z-10 left-0 right-0 w-full top-16 bg-base-100 p-2">
-                {items.map((item) => (
-                    <li key={item.title}>
-                        <Link href={item.slag} onClick={() => setIsOpen(false)}>
-                            {item.title}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        )}
-    </div>
-  )
-}
+        </div>
+    )
+    }
